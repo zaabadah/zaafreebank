a388f057f510df704c383d19fca89a4df080b4e7
zaabadah <zaabadah.moraes@gmail.com>
2022-05-09 12:01:30 -0300

    Primeiro commit

---
+++ zaafreebank/src/main/java/com/zaafreebank/zaafreebank/ZaafreebankApplication.java
@@ -0,0 +1,13 @@
+package com.zaafreebank.zaafreebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaafreebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaafreebankApplication.class, args);
+	}
+
+}
